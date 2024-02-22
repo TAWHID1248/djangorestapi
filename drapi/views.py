bb5490526305fed3716e56dc024d272ec86dbe5c
tawhid1248 <frzfardin2020@gmail.com>
2024-02-22 13:12:25 +0600

    api update

--- drapi/views.py
+++ drapi/views.py
@@ -49,3 +49,23 @@ def aiquest_create(request):
             return HttpResponse(res, content_type='application/json')
         json_data = JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data, content_type='application/json')
+    
+
+    if request.method == 'PUT':
+        json_data = request.body
+        #json to stream python 
+        stream = io.BytesIO(json_data)
+        #stream to python
+        pythondata = JSONParser().parse(stream)
+        #python to complex
+        serializer = AiquestSerializer(data=pythondata)
+        id=pythondata.get('id')
+        aiq = Aiquest.objects.get(id=id)
+        serializer = AiquestSerializer(aiq, data=pythondata, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg':'Successfully update data.'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(res, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
